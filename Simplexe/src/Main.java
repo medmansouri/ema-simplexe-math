92ab4c7c97c02f13d791953063f422264d9f4e88
nico.tomio <nico.tomio@gmail.com>
2011-05-17 12:10:19 +0000

    testSimplexe

---
+++ Simplexe/src/Main.java
@@ -0,0 +1,13 @@
+
+
+import Interface.InterfaceAccueil;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterfaceAccueil accueil = new InterfaceAccueil();
+	}
+
+}
